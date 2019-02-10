1cf58bf5bc8edbcde4de750a9a645577c20e1d82
zaboltar <cletothar@gmail.com>
2019-02-09 23:33:40 -0500

    1 de 2

---
+++ DevilishResearch/Assets/InfoNotes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoNotes : MonoBehaviour {
+
+    /*
+      https://www.youtube.com/watch?v=Zx4r7LlCaQc&list=PLaKASd7MJVnzlqFAnOndK4gJ2aXp--23J&index=12
+      no hago particleFX spawn
+      hacer cap 11 special attacks
+     * */
+}
